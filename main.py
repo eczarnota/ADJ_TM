09ad7fd73ccd5c73d009eb209d66b808c09d6f8d
eczarnota <ewelina.czarnota@edu.uekat.pl>
2022-05-22 10:24:19 +0200

    commit2

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import (
     CountVectorizer
 )
+
+
 df_raiting = pd.read_csv(r"C:\Users\EC\TextMining\alexa_reviews.csv", header=0, names=['rating'])
 df_verification = pd.read_csv(r"C:\Users\EC\TextMining\alexa_reviews.csv", header=0, names=['verified_reviews'])
-df_raiting["dataset"] = 1
-df_verification["dataset"] = 4
 
+df_joined = pd.concat([df_raiting, df_verification])
